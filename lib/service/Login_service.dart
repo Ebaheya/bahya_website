684fb71d374121fd7dd14984127e2a7981af1caa
Amr Hamouda <amr.hamouda@gmail.com>
2026-04-25 15:24:44 +0300

    Implement safeRequest function for token refresh handling and improve error management

--- lib/service/Login_service.dart
+++ lib/service/Login_service.dart
@@ -1,10 +1,10 @@
+import 'dart:async';
 import 'dart:developer';
 import 'package:bahya_website/helper/strings.dart';
 import 'package:dio/dio.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 final Dio dio = Dio();
-
 Future<void> login({required String email, required String password}) async {
   try {
     final response = await dio.post(
@@ -70,3 +70,53 @@ Future<void> clearTokens() async {
     log('Error clearing tokens: $e');
   }
 }
+Future<Response> safeRequest(
+  Future<Response> Function(String? token) request,
+) async {
+  final prefs = await SharedPreferences.getInstance();
+  String? accessToken = prefs.getString('accessToken');
+
+  try {
+    return await request(accessToken);
+  } on DioException catch (e) {
+    if (e.response?.statusCode == 401) {
+      try {
+        final refreshToken = prefs.getString('refreshToken');
+
+        final refreshResponse = await dio.post(
+          '$baseUrl/auth/refresh',
+          data: {"refreshToken": refreshToken},
+        );
+
+        if (refreshResponse.statusCode == 200) {
+          accessToken = refreshResponse.data['accessToken'];
+          final newRefreshToken = refreshResponse.data['refreshToken'];
+
+          await prefs.setString('accessToken', accessToken!);
+          await prefs.setString('refreshToken', newRefreshToken);
+
+          return await request(accessToken);
+        } else {
+          throw Exception("Refresh failed");
+        }
+      } catch (_) {
+        await prefs.remove('accessToken');
+        await prefs.remove('refreshToken');
+        rethrow;
+      }
+    }
+    rethrow;
+  }
+}
+
+// final response = await safeRequest((token) {
+//   return dio.get(
+//     '/profile',
+//     options: Options(
+//       headers: {
+//         'Authorization': 'Bearer $token',
+//       },
+//     ),
+//   );
+// });
+
